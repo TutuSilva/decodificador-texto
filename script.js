9fa5cf477a8be903cc88ab3fc683286490245626
TutuSilva <160740774+TutuSilva@users.noreply.github.com>
2024-03-05 08:17:02 -0300

    node files

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
-    function criptografia(){
-        let textoBase = document.querySelector("#main__section__text__id").value;
-        let campoTextoBase = document.querySelector("#main__section__textbox__textarea__id")
-        let image = document.querySelector("#main__section__textbox__")
-        campoTextoBase.value = textoBase;
-
-    }
+function criptografia() {
+  let textoBase = document.querySelector("#main__section__text__id").value;
+  let campoTextoBase = document.querySelector(
+    "#main__section__textbox__textarea__id"
+  );
+  let image = document.querySelector("#main__section__textbox__");
+}
