da01f631403702e527efa2a2dd677e122f2f4b80
TutuSilva <160740774+TutuSilva@users.noreply.github.com>
2024-03-05 11:00:22 -0300

    criptografar feito

--- script.js
+++ script.js
@@ -1,7 +1,29 @@
-function criptografia() {
-  let textoBase = document.querySelector("#main__section__text__id").value;
-  let campoTextoBase = document.querySelector(
-    "#main__section__textbox__textarea__id"
-  );
-  let image = document.querySelector("#main__section__textbox__");
+const textoDigitado = document.querySelector("#main__section__text__id");
+const textoDisplay = document.querySelector(
+  "#main__section__textbox__textarea__id"
+);
+
+function btnCriptografia() {
+  const textoEncriptar = criptografia(textoDigitado.value);
+  textoDisplay.value = textoEncriptar;
+}
+
+function criptografia(textoCriptografado) {
+  const arrayCriptografada = [
+    ["e", "enter"],
+    ["i", "imes"],
+    ["a", "ai"],
+    ["o", "ober"],
+    ["u", "ufat"],
+  ];
+  textoCriptografado = textoCriptografado.toLowerCase();
+  for (let i = 0; i < arrayCriptografada.length; i++) {
+    if (textoCriptografado.includes(arrayCriptografada[i][0])) {
+      textoCriptografado = textoCriptografado.replaceAll(
+        arrayCriptografada[i][0],
+        arrayCriptografada[i][1]
+      );
+    }
+  }
+  return textoCriptografado;
 }
