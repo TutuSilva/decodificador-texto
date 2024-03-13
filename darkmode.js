e51ba5cc8cfa3ed003ff2e180e6b0e9eeff19714
TutuSilva <160740774+TutuSilva@users.noreply.github.com>
2024-03-13 10:25:29 -0300

    Projeto finalizado

---
+++ darkmode.js
@@ -0,0 +1,11 @@
+let body = document.querySelector("body");
+let headerTrail = document.querySelector("#header__trail__id");
+let display = document.querySelector(".main__section__textbox__");
+let textoUsuario = document.querySelector("#main__section__text__id");
+
+headerTrail.addEventListener("click", () => {
+  headerTrail.classList.toggle("dark");
+  body.classList.toggle("dark");
+  display.classList.toggle("dark");
+  textoUsuario.classList.toggle("dark");
+});
--- script.js
+++ script.js
@@ -2,20 +2,49 @@ const textoDigitado = document.querySelector("#main__section__text__id");
 const textoDisplay = document.querySelector(
   "#main__section__textbox__textarea__id"
 );
+const imagem = document.querySelector(".main__section__textbox__image");
+const avisoSemMensagem = document.querySelector(
+  ".main__section__textbox__placeholder__subtitle"
+);
+const avisoInstrucao = document.querySelector(
+  ".main__section__textbox__placeholder__paragraph"
+);
+const copiarTexto = document.querySelector(
+  ".main__section__textbox__copybutton"
+);
+
+function btnCopy() {
+  navigator.clipboard.writeText(textoDisplay.value);
+  alert("Texto copiado com sucesso!");
+}
+
+function limpaInfo() {
+  imagem.style.display = "none";
+  avisoSemMensagem.style.display = "none";
+  avisoInstrucao.style.display = "none";
+  copiarTexto.style.display = "block";
+}
 
 function btnCriptografia() {
   const textoEncriptar = criptografia(textoDigitado.value);
-  textoDisplay.value = textoEncriptar;
+  if (textoEncriptar == "") {
+    alert("O campo não pode ficar vazio.");
+  } else {
+    textoDisplay.style.display = "block";
+    textoDisplay.value = textoEncriptar;
+    limpaInfo();
+  }
 }
 
 function criptografia(textoCriptografado) {
   const arrayCriptografada = [
-    ["e", "enter"],
     ["i", "imes"],
+    ["e", "enter"],
     ["a", "ai"],
     ["o", "ober"],
     ["u", "ufat"],
   ];
+
   textoCriptografado = textoCriptografado.toLowerCase();
   for (let i = 0; i < arrayCriptografada.length; i++) {
     if (textoCriptografado.includes(arrayCriptografada[i][0])) {
@@ -31,12 +60,17 @@ function criptografia(textoCriptografado) {
 function btnDescriptografia() {
   const textoDesencriptar = descriptografia(textoDigitado.value);
   textoDisplay.value = textoDesencriptar;
+  if (textoDesencriptar === "") {
+    alert("O campo não pode ficar vazio.");
+  } else {
+    limpaInfo();
+  }
 }
 
 function descriptografia(textoDescriptografado) {
   const arrayCriptografada = [
-    ["enter", "e"],
     ["imes", "i"],
+    ["enter", "e"],
     ["ai", "a"],
     ["ober", "o"],
     ["ufat", "u"],
